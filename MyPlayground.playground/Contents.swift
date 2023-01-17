4c019aa6a26455c9fbc18b1977fd4d12e2337e6c
Upendra Sahu <upendra.sahu@thoughtworks.com>
2023-01-17 17:42:51 +0530

    Variable and Constants

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,18 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+var name = "Upendra Sahu"
+
+var age = 27 //variable value is changed
+
+age
+
+age = 31
+
+age
+
+let numberOfMonths = 12 // let using for constant value
+
+// numberOfMonths value not changed
+
+var candy = "Imli"
 
